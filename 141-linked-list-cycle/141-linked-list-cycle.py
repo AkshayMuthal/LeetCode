44bd10c2f91972e6dbf85e35638707c762d574f3
AkshayMuthal <muthalakshay14@gmail.com>
2022-03-08 15:50:23 +0530

    Time: 68 ms (41.33%), Space: 20.4 MB (65.87%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -5,21 +5,16 @@
 #         self.next = None
 
 class Solution(object):
-    def cycle(self, curr, nxt):
-        if curr == None or nxt == None or nxt.next == None:
-            return False
-        if curr == nxt:
-            return True
-        curr = curr.next
-        nxt = nxt.next.next
-        return self.cycle(curr, nxt)
-        
     def hasCycle(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        if head == None:
-            return False
-        return self.cycle(head, head.next)
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow==fast:
+                return True
+        return False
         
