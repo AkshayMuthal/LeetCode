036c8eba244b513175fb9aa4b64017ef4faa5113
AkshayMuthal <muthalakshay14@gmail.com>
2022-04-08 18:22:19 +0530

    Time: 38 ms (14.08%), Space: 13.5 MB (54.41%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -4,17 +4,16 @@ class Solution(object):
         :type stones: List[int]
         :rtype: int
         """
-        l, r = 0, len(stones)-1
+        r = len(stones)-1
         
         while r>0:
-            stones[l:(r+1)] = sorted(stones[l:(r+1)])
-            print(stones[l:(r+1)])
+            stones[:(r+1)] = sorted(stones[:(r+1)])
+            print(stones[:(r+1)])
 
             if stones[r]==stones[r-1]:
                 r-=1
             else:
-                elem = stones[r]-stones[r-1]
-                stones[r-1]=elem
+                stones[r-1] = stones[r]-stones[r-1]
             r-=1
         if r<0:
             return 0
