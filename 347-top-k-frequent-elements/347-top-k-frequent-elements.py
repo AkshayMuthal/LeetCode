6c9a8f5da369e5a3b0600e02a5b4f5a641d318a9
AkshayMuthal <muthalakshay14@gmail.com>
2022-04-09 13:24:34 +0530

    Time: 108 ms (86.33%), Space: 18.7 MB (68.16%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], ki: int) -> List[int]:
+        mp = {}
+        for i in nums:
+            if i in mp:
+                mp[i]+=1
+            else:
+                mp[i] = 1
+        ans = []
+        mp2 = {k: v for k, v in sorted(mp.items(), key=lambda item: item[1], reverse=True)}
+        # print(mp2)
+        for k, v in mp2.items():
+            if ki<=0:
+                break
+            ans.append(k)
+            ki-=1
+        return ans
