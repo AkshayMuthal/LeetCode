16944b3893edfabab442511443c71b32cf0bd23c
AkshayMuthal <muthalakshay14@gmail.com>
2022-05-07 14:44:19 +0530

    Time: 382 ms (80.86%), Space: 32.2 MB (12.38%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -1,18 +1,11 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        l = len(nums)-1
-        amin = [0 for i in range(l+1)]
-        amax = deque()
-        amin[0] = nums[0]
-
-        for i in range(1, len(nums)):
-            amin[i] = min(amin[i-1], nums[i])
-        
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i]>amin[i]:
-                while len(amax)>0 and amax[-1]<=amin[i]:
-                    amax.pop()
-                if len(amax)>0 and nums[i]>amax[-1]:
-                    return True
-                amax.append(nums[i])
+        s = -10000000000
+        stk = deque()
+        for n in nums[::-1]:
+            if n<s:
+                return True
+            while len(stk)>0 and n>stk[-1]:
+                s = stk.pop()
+            stk.append(n)
         return False
