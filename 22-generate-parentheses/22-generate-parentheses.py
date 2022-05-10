6cd7a9ae64bbc21d8cb7c93edfae721ca48f91cc
AkshayMuthal <muthalakshay14@gmail.com>
2022-05-11 00:03:46 +0530

    Time: 50 ms (48.48%), Space: 14.3 MB (41.50%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,15 +1,16 @@
 class Solution:
-    def add_param(self, o, c, n, s):
-        if o>n or c>n:
+    def add_param(self, o, c, s):
+        if o>self.n or c>self.n:
             return
-        if o+c == 2*n and o==c:
+        if o+c == 2*self.n and o==c:
             self.ans.append(s)
         else:
-            self.add_param(o+1, c, n, s+"(")
+            self.add_param(o+1, c, s+"(")
             if c+1<=o:
-                self.add_param(o, c+1, n, s+")")
+                self.add_param(o, c+1, s+")")
     
     def generateParenthesis(self, n: int) -> List[str]:
         self.ans = []
-        self.add_param(0, 0, n, "")
+        self.n = n
+        self.add_param(0, 0, "")
         return self.ans
