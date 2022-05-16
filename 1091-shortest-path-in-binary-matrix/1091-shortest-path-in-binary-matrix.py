73189a805c2139cf138314010687408e5e1a586c
AkshayMuthal <muthalakshay14@gmail.com>
2022-05-16 13:36:10 +0530

    Time: 658 ms (81.32%), Space: 14.4 MB (63.63%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,40 +5,37 @@ class Solution:
             return -1
         queue = deque()
         queue.append((0, 0, 1))
-        grid[0][0] = 1
+        # grid[0][0] = 1
         
         
-        while len(queue)>0:
-            i, j, k = queue.pop()
-            if i==n-1 and j==n-1:
-                return k
-            for x, y in ((i-1,j-1),(i-1,j),(i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1),(i+1,j),(i+1,j+1)):
-                if 0<=x<n and 0<=y<n and grid[x][y] == 0:
-                    grid[x][y] = 1
-                    queue.appendleft((x, y, k+1))
-        return -1
+        # while len(queue)>0:
+        #     i, j, k = queue.pop()
+        #     if i==n-1 and j==n-1:
+        #         return k
+        #     for x, y in ((i-1,j-1),(i-1,j),(i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1),(i+1,j),(i+1,j+1)):
+        #         if 0<=x<n and 0<=y<n and grid[x][y] == 0:
+        #             grid[x][y] = 1
+        #             queue.appendleft((x, y, k+1))
+        # return -1
 
     
-#         while len(queue)>0:
-#             i, j, k = queue.pop()
+        while len(queue)>0:
+            i, j, k = queue.pop()
+            if i<0 or i>=n or j<0 or j>=n or grid[i][j] == 1:
+                continue
+                
+            if i==n-1 and j==n-1:
+                return k
             
-#             if i<0 or j>=n or i>=n or j<0 or grid[i][j] == 1:
-#                 continue
-#             if i==n-1 and j==n-1:
-#                 mind = min(mind, k+1)
-#                 continue
+            grid[i][j] = 1
             
-#             grid[i][j] = 1
-            
-#             queue.append((i+1, j, k+1))
-#             queue.append((i-1, j, k+1))
-#             queue.append((i-1, j-1, k+1))
-#             queue.append((i-1, j+1, k+1))
-#             queue.append((i, j+1, k+1))
-#             queue.append((i, j-1, k+1))
-#             queue.append((i+1, j-1, k+1))
-#             queue.append((i+1, j+1, k+1))
+            queue.appendleft((i-1, j-1, k+1))
+            queue.appendleft((i-1, j, k+1))
+            queue.appendleft((i-1, j+1, k+1))
+            queue.appendleft((i, j-1, k+1))
+            queue.appendleft((i, j+1, k+1))
+            queue.appendleft((i+1, j-1, k+1))
+            queue.appendleft((i+1, j, k+1))
+            queue.appendleft((i+1, j+1, k+1))
         
-#         if mind == maxx:
-#             return -1
-#         return mind
+        return -1
