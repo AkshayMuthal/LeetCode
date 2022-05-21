14463b3abc6c507dac23e3cf8da01f244091f262
AkshayMuthal <muthalakshay14@gmail.com>
2022-05-21 14:02:53 +0530

    Time: 49 ms (71.66%), Space: 14 MB (42.36%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -6,25 +6,17 @@ class Solution:
         if obstacleGrid[m-1][n-1] == 1 or obstacleGrid[0][0] == 1:
             return 0
         
-        for i in range(m):
-            for j in range(n):
-                if obstacleGrid[i][j] == 1:
-                    obstacleGrid[i][j] = -1
-
-        obstacleGrid[m-1][n-1] = 1
+        obstacleGrid[m-1][n-1] = -1
         for i in range(n-2, -1, -1):
-            obstacleGrid[m-1][i] = obstacleGrid[m-1][i+1] if obstacleGrid[m-1][i]>=0 else obstacleGrid[m-1][i]
+            obstacleGrid[m-1][i] = obstacleGrid[m-1][i+1] if obstacleGrid[m-1][i]<=0 else obstacleGrid[m-1][i]
             
         for i in range(m-2, -1, -1):
-            obstacleGrid[i][n-1] = obstacleGrid[i+1][n-1] if obstacleGrid[i][n-1]>=0 else obstacleGrid[i][n-1]
-            
-        # print(obstacleGrid)
-            
+            obstacleGrid[i][n-1] = obstacleGrid[i+1][n-1] if obstacleGrid[i][n-1]<=0 else obstacleGrid[i][n-1]
+                        
         for i in range(m-2, -1, -1):
             for j in range(n-2, -1, -1):
-                if obstacleGrid[i][j]!=-1:
-                    obstacleGrid[i][j] += obstacleGrid[i+1][j] if obstacleGrid[i+1][j]>=0 else 0
-                    obstacleGrid[i][j] += obstacleGrid[i][j+1] if obstacleGrid[i][j+1]>=0 else 0
-        # print(obstacleGrid)
+                if obstacleGrid[i][j]!=1:
+                    obstacleGrid[i][j] += obstacleGrid[i+1][j] if obstacleGrid[i+1][j]<=0 else 0
+                    obstacleGrid[i][j] += obstacleGrid[i][j+1] if obstacleGrid[i][j+1]<=0 else 0
         
-        return obstacleGrid[0][0]
+        return -obstacleGrid[0][0]
