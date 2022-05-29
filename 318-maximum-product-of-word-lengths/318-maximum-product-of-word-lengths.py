639d94454f1177edd9a59629d9e68964b1affb46
AkshayMuthal <muthalakshay14@gmail.com>
2022-05-29 13:18:04 +0530

    Time: 700 ms (68.83%), Space: 14.3 MB (93.67%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -2,19 +2,15 @@ class Solution:
     def maxProduct(self, words: List[str]) -> int:
         maxcnt = 0
         l = len(words)
+        mask = [0 for _ in range(l)]
         
-        for i in range(l):
-            ca = [0 for _ in range(256)]
-            for c in words[i]:
-                ca[ord(c)]+=1
-            li = len(words[i])
+        for i, word in enumerate(words):
+            for ch in word:
+                mask[i] |= (1 << ord(ch)-ord('a'))
+            li = len(word)
             
-            for j in range(i+1, l):
+            for j in range(i):
                 flag = 0
-                for w in words[j]:
-                    if ca[ord(w)]>0:
-                        flag = 1
-                        break
-                if flag == 0:
+                if (mask[i] & mask[j] == 0):
                     maxcnt = max(maxcnt, li*len(words[j]))
         return maxcnt
