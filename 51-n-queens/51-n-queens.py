14e8cf31e6b1a283f03dafdd708254066503ddda
AkshayMuthal <muthalakshay14@gmail.com>
2022-06-05 14:22:00 +0530

    Time: 810 ms (5.01%), Space: 14.4 MB (84.07%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,66 @@
+import copy
+
+class Solution:
+    def visit_node(self, row, col, n, visited):
+        visited[row][col] = 1
+        
+        for i in range(n):
+            visited[i][col] = 1
+            visited[row][i] = 1
+        
+        i, j = row, col
+        while i<n and j<n:
+            visited[i][j] = 1
+            i+=1
+            j+=1
+        
+        i, j = row, col
+        while i>=0 and j>=0:
+            visited[i][j] = 1
+            i-=1
+            j-=1
+        
+        i, j = row, col
+        while i>=0 and j<n:
+            visited[i][j] = 1
+            i-=1
+            j+=1
+            
+        i, j = row, col
+        while j>=0 and i<n:
+            visited[i][j] = 1
+            i+=1
+            j-=1
+        
+        return visited
+            
+    def set_queen(self, col, row, n, visited, ans):
+        ans.append(self.queen_li[col])
+        visited = self.visit_node(row, col, n , visited)
+        if row == n-1:
+            self.res.append(ans)
+            return
+        
+        for i in range(n):
+            if visited[row+1][i] == 0:
+                new_visited = copy.deepcopy(visited)
+                new_ans = copy.deepcopy(ans)
+                self.set_queen(i, row+1, n, new_visited, new_ans)
+    
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        self.res = []
+        self.queen_li = []
+        for i in range(n):
+            ans = ""
+            for j in range(n):
+                if j == i:
+                    ans+="Q"
+                else:
+                    ans+="."
+            self.queen_li.append(ans)
+        
+        for col in range(n):
+            visited = [[0 for _ in range(n)] for _ in range(n)]
+            self.set_queen(col, 0, n, visited, [])
+        
+        return self.res
