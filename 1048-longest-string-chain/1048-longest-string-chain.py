c65567cee3938b171fdcf052a2446e61fdeb43b1
AkshayMuthal <muthalakshay14@gmail.com>
2022-06-15 16:39:43 +0530

    Time: 2463 ms (11.60%), Space: 14.2 MB (88.74%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,19 +1,20 @@
 class Solution:    
-    def getdist(self, word, d, hm , words):
-        maxd = d
+    def getdist(self, word, hm , words):
+        maxd = 1
         for i in range(len(word)):
             sub_word = word[:i] + word[i+1:]
             if sub_word in words:
                 if sub_word not in hm:
-                    hm[sub_word] = self.getdist(sub_word, 1, hm , words)
-                maxd = max(maxd, d+hm[sub_word])
+                    hm[sub_word] = self.getdist(sub_word, hm , words)
+                maxd = max(maxd, 1+hm[sub_word])
         return maxd
     
     
     def longestStrChain(self, words: List[str]) -> int:
         maxd, hm = 1, {}
         for word in words:
-            hm[word] = self.getdist(word, 1, hm, words)
-        for k, v in hm.items():
-            maxd = max(maxd, v)
+            if maxd<=len(word):
+                hm[word] = self.getdist(word, hm, words)
+                maxd = max(maxd, hm[word])
         return maxd
+
