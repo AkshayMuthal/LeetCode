07de7c827c28b23d2625e14c67bb35c037c804e7
AkshayMuthal <muthalakshay14@gmail.com>
2022-06-15 18:29:07 +0530

    Time: 204 ms (67.19%), Space: 14.2 MB (88.74%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,16 +1,11 @@
 class Solution:    
-    def getdist(self, word, hm , words):
-        maxd = 1
-        for i in range(len(word)):
-            sub_word = word[:i] + word[i+1:]
-            maxd = max(maxd, hm.get(sub_word, 0)+1)
-        return maxd
-    
-    
     def longestStrChain(self, words: List[str]) -> int:
         maxd, hm = 1, {}
         for word in sorted(words, key=len):
-            hm[word] = self.getdist(word, hm, words)
+            hm[word] = 1
+            for i in range(len(word)):
+                sub_word = word[:i] + word[i+1:]
+                hm[word] = max(hm[word], hm.get(sub_word, 0)+1)
             maxd = max(maxd, hm[word])
         return maxd
 
