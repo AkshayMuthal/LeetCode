6cfd6fa713f4f2c3b44a9b20fe6062e850faee2c
AkshayMuthal <muthalakshay14@gmail.com>
2022-06-23 18:57:53 +0530

    Time: 1499 ms (83.51%), Space: 14.2 MB (69.46%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -2,18 +2,15 @@ class Solution:
     
     def coinChange(self, coins: List[int], amount: int) -> int:
         l = len(coins)
-        dp = [[amount+1 for _ in range(l+1)] for _ in range(amount+1)]
+        dp = [amount+1 for _ in range(amount+1)]
+        dp[0] = 0
+        coins.sort()
         
-        for i in range(amount+1):
-            for j in range(l+1):
-                if i ==0 or j ==0:
-                    dp[i][j] = 0 if i==0 else sys.maxsize
+        for amt in range(1, amount+1):
+            for coin in coins:
+                if amt >= coin:
+                    dp[amt] = min(dp[amt], 1+dp[amt-coin])
+                else:
+                    break
         
-        
-        for i in range(1, amount+1):
-            for j in range(1, l+1):
-                dp[i][j] = dp[i][j-1]
-                if coins[j-1]<=i:
-                    dp[i][j] = min(dp[i][j], 1+dp[i-coins[j-1]][j])
-        
-        return dp[amount][l] if dp[amount][l] != sys.maxsize else -1
+        return dp[amount] if dp[amount] != amount+1 else -1
