8acbc6b1a7b83e8ada98aafd2a26e72824b78eb4
AkshayMuthal <muthalakshay14@gmail.com>
2022-06-25 17:37:27 +0530

    Time: 247 ms (11.27%), Space: 17.8 MB (57.62%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -4,22 +4,16 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        def get_str(s):
-            char = [0]*26
-            for i in s:
-                char[ord(i)-97] += 1
-            
-            ans = ""
-            for i in range(26):
-                ans = ans + "#" + str(char[i])
-            return ans
-        
         hm = {}
-        
         for i in range(len(strs)):
-            s = get_str(strs[i])
+            char = [0]*26
+            for j in strs[i]:
+                char[ord(j)-97] += 1
+            s = ""
+            for j in range(26):
+                s = s + "#" + str(char[j])
             if s not in hm:
-                hm[s] = []
+                hm[s] = []            
             hm[s].append(strs[i])
         
         return hm.values()
