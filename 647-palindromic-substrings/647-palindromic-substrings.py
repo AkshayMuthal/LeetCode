2dd53d3464611d8c646ca7020ca741a5cc3db397
AkshayMuthal <muthalakshay14@gmail.com>
2022-06-25 19:13:38 +0530

    Time: 123 ms (70.67%), Space: 13.6 MB (37.53%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,10 +1,21 @@
-class Solution:
-    def countSubstrings(self, s: str) -> int:
-        n = len(s)
-        res = 0
-        pal = [[0]*n for _ in range(n)]
-        for i in range(n, -1, -1):
-            for j in range(i, n):
-                pal[i][j] = s[i] == s[j] and (j-i+1<3 or pal[i+1][j-1])
-                res+=pal[i][j]
-        return res
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = len(s)
+        self.count = 0
+        
+        def get_pallindrome(c, nc): 
+            while c>=0 and nc < l and s[c] == s[nc]:
+                self.count+=1
+                c-=1
+                nc+=1  
+        
+        for i in range(l):
+            get_pallindrome(i, i)             
+            get_pallindrome(i, i+1)
+            
+        return self.count
+    
