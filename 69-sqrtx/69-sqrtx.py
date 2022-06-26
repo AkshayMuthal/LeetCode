d85a99b532129ec7b15c7e1fd4e54e5dc7448106
AkshayMuthal <muthalakshay14@gmail.com>
2022-06-26 21:43:38 +0530

    Time: 65 ms (23.82%), Space: 13.3 MB (83.24%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -4,12 +4,14 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        n = 1
-        while True:
-            sqr = n*n
+        l, r = 1, x
+        while l<=r:
+            mid = int(l + (r-l)/2)
+            sqr = mid*mid
             if sqr == x:
-                return n
-            if sqr > x:
-                return n - 1
-            n += 1
-        
+                return mid
+            elif sqr > x:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l-1
