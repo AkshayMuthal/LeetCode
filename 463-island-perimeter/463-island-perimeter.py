099f7e054d0ae4b6cf04afc0d650057861e39d3d
AkshayMuthal <muthalakshay14@gmail.com>
2022-06-29 00:43:01 +0530

    Time: 693 ms (41.70%), Space: 13.6 MB (92.42%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -7,22 +7,11 @@ class Solution(object):
         rl = len(grid)
         cl = len(grid[0])
         
-        def get_perimeter(i, j):
-            # print(i, j)
-            if i<0 or i>=rl or j<0 or j>=cl or grid[i][j] == 0:
-                return 1
-            if grid[i][j] == -1:
-                return 0
-            
-            grid[i][j] = -1
-            perimeter = 0 
-            for r, c in [(i+1, j), (i-1, j), (i, j-1), (i, j+1)]:
-                perimeter += get_perimeter(r, c)
-            
-            # print("perimeter: ", perimeter)
-            return perimeter
-        
+        perimeter = 0
         for i in range(rl):
             for j in range(cl):
                 if grid[i][j] == 1:
-                    return get_perimeter(i, j)
+                    for r, c in [(i+1, j), (i-1, j), (i, j-1), (i, j+1)]:
+                        if r<0 or r>=rl or c<0 or c>=cl or grid[r][c] == 0:
+                            perimeter += 1
+        return perimeter
