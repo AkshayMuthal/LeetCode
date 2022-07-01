1df0a006ac16236014230493259ecce504ce5bd3
AkshayMuthal <muthalakshay14@gmail.com>
2022-07-01 23:01:20 +0530

    Time: 128 ms (27.52%), Space: 26 MB (65.81%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -5,23 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def inorder(self, root):
-        if root == None:
-            return 0
-        
-        left_sum = max(self.inorder(root.left), 0)
-        right_sum = max(self.inorder(root.right), 0)
-        
-        curr_sum = root.val + left_sum + right_sum
-        self.max_sum = max(self.max_sum, curr_sum)
-        
-        return root.val + max(left_sum, right_sum)
-    
     def maxPathSum(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
         self.max_sum = -1001
-        self.inorder(root)
+        def inorder(root):
+            if root == None:
+                return 0
+
+            left_sum = max(inorder(root.left), 0)
+            right_sum = max(inorder(root.right), 0)
+
+            curr_sum = root.val + left_sum + right_sum
+            self.max_sum = max(self.max_sum, curr_sum)
+
+            return root.val + max(left_sum, right_sum)
+        
+        inorder(root)
         return self.max_sum
