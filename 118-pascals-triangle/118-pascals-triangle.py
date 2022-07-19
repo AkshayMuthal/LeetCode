0eca5125dc4e369b3c89683d685c9d753d0a0525
AkshayMuthal <muthalakshay14@gmail.com>
2022-07-19 13:54:15 +0530

    Time: 23 ms (67.50%), Space: 13.4 MB (35.53%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,13 +1,16 @@
-class Solution:
-    def __init__(self):
-        self.li = [[1],[1,1]]
-        for i in range(2, 31):
-            ans = [1]
-            prev = self.li[i-1]
-            for i in range(len(prev)-1):
-                ans.append(prev[i]+prev[i+1])
-            ans.append(1)
-            self.li.append(ans)
-        
-    def generate(self, numRows: int) -> List[List[int]]:
-        return self.li[0:numRows]
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        ans = [[1]]
+        prev = [1]
+        for i in range(1, numRows):
+            curr = [1]
+            for j in range(1, len(prev)):
+                curr.append(prev[j]+prev[j-1])
+            curr.append(1)
+            prev = curr
+            ans.append(curr)
+        return ans
