e10c537606ad8f2ca080ca891562b0ee7894f6d0
AkshayMuthal <muthalakshay14@gmail.com>
2022-07-21 23:27:46 +0530

    Time: 9445 ms (5.02%), Space: 17.5 MB (12.54%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,21 +1,25 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         ans = set()
-        m = defaultdict(set)
-        for i in range(len(nums)):
-            m[nums[i]].add(i)
         
-        for i in m:
-            for j in m:
-                if i == j and len(m[i])<2:
-                    continue
-                target = -i-j
-                if target in m:
-                    if target == i and len(m[i])<2:
+        hm = defaultdict()
+        for i in range(len(nums)):
+            hm[nums[i]] = hm.get(nums[i], []) + [i]
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                target = -(nums[i]+nums[j])
+                if target in hm:
+                    if target == nums[i] and len(hm[target])<2:
                         continue
-                    if target == j and len(m[j])<2:
+                    if target == nums[j] and len(hm[target])<2:
                         continue
-                    if target == i == j and len(m[i])<3:
+                    if target == nums[i] == nums[j] and len(hm[target])<3:
                         continue
-                    ans.add(tuple(sorted([i, j, target])))
+                    ans.add(tuple(sorted([nums[i], nums[j], target])))
+        
         return ans
