87d5098a1dc32c1f6fb1ae401d931f889c332862
AkshayMuthal <muthalakshay14@gmail.com>
2022-07-24 01:29:28 +0530

    Time: 19 ms (93.59%), Space: 13.4 MB (74.68%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -6,55 +6,18 @@ class Solution(object):
         :rtype: str
         """
         i, j, c = len(a)-1, len(b)-1, 0
-        
-        # 0+0+0 = 0, c=0
-        # 1+0+0 = 1, c=0
-        # 1+1+0 = 0, c=1
-        # 1+1+1 = 1, c=1
         ans = ""
         
-        while i>=0 and j>=0:
-            va, vb = int(a[i]), int(b[j])
-            if va+vb+c == 0:
-                c = 0
-                ans = "0" + ans
-            elif va+vb+c == 1:
-                c = 0
-                ans = "1" + ans
-            elif va+vb+c == 2:
-                c = 1
-                ans = "0" + ans
-            elif va+vb+c == 3:
-                c = 1
-                ans = "1" + ans
-            i-=1
-            j-=1
-        
-        while i>=0:
-            va = int(a[i])
-            if va+c == 0:
-                c = 0
-                ans = "0" + ans
-            elif va+c == 1:
-                c = 0
-                ans = "1" + ans
-            elif va+c == 2:
-                c = 1
-                ans = "0" + ans
-            i-=1
-        
-        while j>=0:
-            vb = int(b[j])
-            if vb+c == 0:
-                c = 0
-                ans = "0" + ans
-            elif vb+c == 1:
-                c = 0
-                ans = "1" + ans
-            elif vb+c == 2:
-                c = 1
-                ans = "0" + ans
-            j-=1
+        while i>=0 or j >= 0:
+            s = c
+            if i>=0:
+                s += int(a[i])
+                i -= 1
+            if j>=0:
+                s += int(b[j])
+                j -= 1
+            ans = str(s%2) + ans
+            c = s/2
         
         if c == 1:
             ans = "1" + ans
