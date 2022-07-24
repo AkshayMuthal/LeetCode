782aa03860504a7d5dc9a8030bc24526daf70a32
AkshayMuthal <muthalakshay14@gmail.com>
2022-07-24 20:18:23 +0530

    Time: 232 ms (32.64%), Space: 19.5 MB (44.28%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -5,14 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        i, j = 0, len(matrix[0])-1 
+        def bs(arr, l, r, target):
+            while l<=r:
+                mid = l + (r-l)//2
+                if arr[mid] == target:
+                    return True
+                elif arr[mid] > target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return False
         
-        while i<len(matrix) and j>=0:
-            val = matrix[i][j]
-            if val == target:
+        h = len(matrix[0])
+        for i in range(len(matrix)):
+            if bs(matrix[i], 0, h-1, target):
                 return True
-            elif val < target:
-                i += 1
-            else:
-                j -= 1
         return False
