cc8a92381e34546735d34b6f8ffa0ab5dc0fa27c
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-06 12:43:03 +0530

    Time: 32 ms (62.95%), Space: 13.5 MB (44.44%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -7,20 +7,22 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        i1, i2 = m-1, n-1
-        for i in range(m+n-1, -1, -1):
-            if i1>=0 and i2>=0:
-                if nums1[i1] > nums2[i2]:
-                    nums1[i] = nums1[i1]
-                    i1-=1
-                else:
-                    nums1[i] = nums2[i2]
-                    i2-=1
+        i1, i2, i = m-1, n-1, m+n-1
+        while i1>=0 and i2>=0:
+            if nums1[i1] > nums2[i2]:
+                nums1[i] = nums1[i1]
+                i1-=1
             else:
-                if i1>=0:
-                    nums1[i] = nums1[i1]
-                    i1-=1
-                elif i2>=0:
-                    nums1[i] = nums2[i2]
-                    i2-=1
+                nums1[i] = nums2[i2]
+                i2-=1
+            i -= 1
+        while i2>=0:
+            nums1[i] = nums2[i2]
+            i2-=1
+            i-=1
+        while i1>=0:
+            nums1[i] = nums1[i1]
+            i1-=1
+            i-=1
+
         
