0ea563f5e7a32335676053fdb28ca4c0974df47d
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-13 18:56:15 +0530

    Time: 18 ms (86.17%), Space: 13.4 MB (82.76%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,22 +1,45 @@
 class Solution(object):
+    def lps(self, pat):
+        lps = [0]*len(pat)
+        i = 1
+        l = 0
+        while i<len(pat):
+            if pat[i] == pat[l]:
+                l += 1
+                lps[i] = l
+                i += 1
+            else:
+                if l == 0:
+                    lps[i] = 0
+                    i += 1
+                else:
+                    l = lps[l-1]
+        return lps
+    
     def strStr(self, haystack, needle):
         """
         :type haystack: str
         :type needle: str
         :rtype: int
         """
-        if len(needle)>len(haystack):
+        if len(haystack)<len(needle):
             return -1
-        if len(needle) == 0:
+        if len(needle)==0:
             return 0
         
-        for i in range(len(haystack)):
-            found = True
-            for j in range(len(needle)):
-                if i+j>=len(haystack) or haystack[i+j] != needle[j]:
-                    found = False
-                    break
-            if found:
-                return i
-        return -1
+        lps = self.lps(needle)
+        i, j = 0, 0
         
+        while i<len(haystack):
+            if haystack[i] == needle[j]:
+                i+=1
+                j+=1
+            if j == len(needle):
+                return i-j
+                # j = lps[j-1]
+            elif i<len(haystack) and haystack[i]!=needle[j]:
+                if j==0:
+                    i+=1
+                else:
+                    j = lps[j-1]
+        return -1
