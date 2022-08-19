8b717ee8c829c61af932defd5fbe4e87b4cc95ff
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-19 18:37:04 +0530

    Time: 35 ms (96.77%), Space: 21.4 MB (8.93%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -1,21 +1,32 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    
-    def inorder(self, root):
-        if root and self.ans==None:
-            self.inorder(root.left)
-            self.k-=1
-            if self.k==0:
-                self.ans = root
-            self.inorder(root.right)
-    
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        def k_small(node, k, ind):
+            if self.ans != None:
+                return self.ans
+            if not node:
+                return ind
+            
+            ind = k_small(node.left, k, ind) + 1
+            if self.ans != None:
+                return self.ans
+            
+            if ind == k:
+                self.ans = node.val
+                return self.ans
+            
+            return k_small(node.right, k, ind)
+        
         self.ans = None
-        self.k = k
-        self.inorder(root)
-        return self.ans.val
+        return k_small(root, k, 0)
+        
