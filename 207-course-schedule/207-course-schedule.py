121cba6ee52a6a646a02ff53cb49bc1c3309274a
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-22 23:31:25 +0530

    Time: 133 ms (38.06%), Space: 14.8 MB (94.08%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,35 @@
+import heapq
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        adj_map = {}
+        indegree = [0]*numCourses
+        
+        for i in range(numCourses):
+            adj_map[i] = []
+            
+        for c_after, c_before in prerequisites:
+            adj_map[c_before] = adj_map.get(c_before) + [c_after]
+            indegree[c_after] += 1
+        
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        while q:
+            course = q.pop()
+            for c_after in adj_map[course]:
+                indegree[c_after] -= 1
+                if indegree[c_after] == 0:
+                    q.append(c_after)
+        
+        for i in indegree:
+            if i != 0:
+                return False
+        return True
