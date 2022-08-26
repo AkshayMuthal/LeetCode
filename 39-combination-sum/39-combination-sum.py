e3974c7258462d1179621ab825119a50bccde26e
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-26 23:53:40 +0530

    Time: 110 ms (52.90%), Space: 13.4 MB (89.06%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,12 +1,12 @@
 class Solution(object):
-    def dfs(self, target, path, res, candidates):
+    def dfs(self, target, path, res, candidates, ind):
         if target < 0:
             return res
         if target == 0:
             res.append(path)
             return res
-        for i in range(len(candidates)):
-            res = self.dfs(target - candidates[i], path + [candidates[i]], res, candidates[i:])
+        for i in range(ind, len(candidates)):
+            res = self.dfs(target - candidates[i], path + [candidates[i]], res, candidates, i)
         return res
     
     def combinationSum(self, candidates, target):
@@ -15,4 +15,5 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        return self.dfs(target, [], [], candidates)
+        # candidates.sort()
+        return self.dfs(target, [], [], candidates, 0)
