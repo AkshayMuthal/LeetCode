0aea8da026d1cbbce8b210029469201a0db4a23b
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-26 12:52:50 +0530

    Time: 43 ms (57.89%), Space: 13.5 MB (42.11%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -7,7 +7,7 @@ class Solution(object):
         counter = collections.Counter(str(n))
         
         for i in range(31):
-            cnt = collections.Counter(str(2**i))
+            cnt = collections.Counter(str(1 << i))
             if cnt == counter:
                 return True
         return False
