c94855712806983bff9a6dcc4305fb61a2dae7a2
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-27 17:39:53 +0530

    Time: 574 ms (95.11%), Space: 27.6 MB (75.68%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -17,11 +17,11 @@ class Solution(object):
             
             for end in range(start, len(s)):
                 if s[start] == s[end] and (end-start<=2 or dp[start+1][end-1]):
-                    dp[start][end] = True
+                    dp[start][end] = 1
                     dfs(end+1, s, dp, ans, cl+[s[start:end+1]])
         
         
-        dp = [[False]*len(s) for _ in range(len(s))]
+        dp = [[0]*len(s) for _ in range(len(s))]
         ans = []
         dfs(0, s, dp, ans, [])
         return ans
