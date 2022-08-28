5698f7192ef93c163f12da5b1b0837d4cc75dd93
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-28 11:44:21 +0530

    Time: 66 ms (16.26%), Space: 14 MB (46.47%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -2,25 +2,18 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if s[0] == "0":
             return 0
-        dp = [0]*len(s)
         
-        def rec(ind, s, cnt, dp):
-            if ind == len(s):
-                cnt += 1
-                return cnt
-            if ind > len(s) or s[ind] == "0":
-                return cnt
-            if dp[ind]!= 0 :
-                cnt += dp[ind]
-                return cnt
+        dp = [0]*(len(s)+1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == "0" else 1
+        
+        for i in range(2, len(s)+1):
+            first = int(s[i-1:i])
+            sec = int(s[i-2:i])
             
-            cnt = rec(ind+1, s, cnt, dp)
-            if ind+2 <= len(s) and int(s[ind:ind+2])<27:
-                cnt = rec(ind+2, s, cnt, dp)
-            
-            dp[ind] = cnt
-            return cnt
-
-        cnt = 0
-        cnt = rec(0, s, cnt, dp)
-        return cnt
+            if 0<first<=9:
+                dp[i] += dp[i-1]
+            if 10<=sec<27:
+                dp[i] += dp[i-2]
+        
+        return dp[len(s)]
