60d67fe3ecfdb115d438f7b8de9314ae640f15e5
AkshayMuthal <muthalakshay14@gmail.com>
2022-08-28 11:44:54 +0530

    Time: 32 ms (95.22%), Space: 13.9 MB (46.47%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -3,11 +3,12 @@ class Solution:
         if s[0] == "0":
             return 0
         
-        dp = [0]*(len(s)+1)
+        n= len(s)
+        dp = [0]*(n+1)
         dp[0] = 1
         dp[1] = 0 if s[0] == "0" else 1
         
-        for i in range(2, len(s)+1):
+        for i in range(2, n+1):
             first = int(s[i-1:i])
             sec = int(s[i-2:i])
             
@@ -16,4 +17,4 @@ class Solution:
             if 10<=sec<27:
                 dp[i] += dp[i-2]
         
-        return dp[len(s)]
+        return dp[n]
