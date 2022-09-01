e14926142c4a776a422b6de42dfb8fd75d9fd709
AkshayMuthal <muthalakshay14@gmail.com>
2022-09-01 11:18:04 +0530

    Time: 586 ms (6.85%), Space: 49.3 MB (46.30%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -1,33 +1,24 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        self.count = 0
-        
-        q = deque()
-        max_r = -10000
-        
-        while root:
-            max_r = max(max_r, root.val)
-            q.append((root, max_r))
-            if root.val>=max_r:
-                self.count+=1
-            root = root.left
-        
-        while len(q)>0:
-            elem, max_r = q.pop()
-            elem = elem.right
-            
-            while elem:
-                max_r = max(max_r, elem.val)
-                q.append((elem, max_r))
-                if elem.val>=max_r:
-                    self.count+=1
-                elem = elem.left
-            
-        return self.count
+class Solution(object):
+    def inorder(self, node, count, max_val):
+        if not node:
+            return count
+        if node.val >= max_val:
+            max_val = node.val
+            count += 1
+        count = self.inorder(node.left, count, max_val)
+        count = self.inorder(node.right, count, max_val)
+        return count
+    
+    def goodNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        count = 0
+        return self.inorder(root, count, root.val)
